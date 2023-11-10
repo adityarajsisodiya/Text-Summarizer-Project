f0865fa3522a4f23974a90b008c8d5e598dab83d
Aditya Raj Sisodiya <sisodiyaadiya95@gmail.com>
2023-11-10 13:28:59 +0530

    commting data ingestion code

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -33,23 +33,18 @@ def read_yaml(path_to_yaml:Path) -> ConfigBox:
 
 
 
-
-
 @ensure_annotations
-def create_directories(path_to_directories : list,verbose=True):
+def create_directories(path_to_directories: list, verbose=True):
     """create list of directories
 
-    Args
-        path_to_directories (list): list of path of directories to create
-        ignore_log (bool,optional): ignore if multiple dirs is to be created.Default is False
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
     for path in path_to_directories:
-        os.makedirs(path,exist_ok=True)
+        os.makedirs(path, exist_ok=True)
         if verbose:
-            logger.info(f"created directory at:{path}")
-
-
-
+            logger.info(f"created directory at: {path}")
 
 @ensure_annotations
 def get_size(path: Path) -> str:
